da33fabdfaa9e9010e6bbce0c573fc35854316bc
Максим Глушков <61595112+maxglushkov@users.noreply.github.com>
2021-03-22 09:54:23 +0300

    Оптимизация invert для необратимых матриц и потенциальных изменений типов

--- lab01/invert/matrix-3x3.cpp
+++ lab01/invert/matrix-3x3.cpp
@@ -1,7 +1,7 @@
 #include <sstream>
 #include "matrix-3x3.h"
 
-bool ReadMatrix(std::istream & input, Matrix3x3 matrix)
+bool ReadMatrix(std::istream & input, Matrix3x3 & matrix)
 {
 	std::string row;
 	for (size_t y = 0; y < MATRIX_SIDE; ++y)
@@ -24,7 +24,7 @@ bool ReadMatrix(std::istream & input, Matrix3x3 matrix)
 	return true;
 }
 
-bool WriteMatrix(std::ostream & output, const Matrix3x3 matrix)
+bool WriteMatrix(std::ostream & output, Matrix3x3 const& matrix)
 {
 	size_t x = 0, y = 0;
 	while (y < MATRIX_SIDE)
@@ -53,23 +53,24 @@ bool WriteMatrix(std::ostream & output, const Matrix3x3 matrix)
 	return true;
 }
 
-bool InvertMatrix(const Matrix3x3 original, Matrix3x3 inverted)
+bool InvertMatrix(Matrix3x3 const& original, Matrix3x3 & inverted)
 {
 	inverted[0][0] = original[1][1] * original[2][2] - original[1][2] * original[2][1];
-	inverted[0][1] = original[0][2] * original[2][1] - original[0][1] * original[2][2];
-	inverted[0][2] = original[0][1] * original[1][2] - original[0][2] * original[1][1];
 	inverted[1][0] = original[1][2] * original[2][0] - original[1][0] * original[2][2];
-	inverted[1][1] = original[0][0] * original[2][2] - original[0][2] * original[2][0];
-	inverted[1][2] = original[0][2] * original[1][0] - original[0][0] * original[1][2];
 	inverted[2][0] = original[1][0] * original[2][1] - original[1][1] * original[2][0];
-	inverted[2][1] = original[0][1] * original[2][0] - original[0][0] * original[2][1];
-	inverted[2][2] = original[0][0] * original[1][1] - original[0][1] * original[1][0];
 
 	const double det = original[0][0] * inverted[0][0] + original[0][1] * inverted[1][0] + original[0][2] * inverted[2][0];
 	if (!det)
 	{
 		return false;
 	}
+
+	inverted[0][1] = original[0][2] * original[2][1] - original[0][1] * original[2][2];
+	inverted[0][2] = original[0][1] * original[1][2] - original[0][2] * original[1][1];
+	inverted[1][1] = original[0][0] * original[2][2] - original[0][2] * original[2][0];
+	inverted[1][2] = original[0][2] * original[1][0] - original[0][0] * original[1][2];
+	inverted[2][1] = original[0][1] * original[2][0] - original[0][0] * original[2][1];
+	inverted[2][2] = original[0][0] * original[1][1] - original[0][1] * original[1][0];
 
 	for (size_t y = 0; y < MATRIX_SIDE; ++y)
 	{
--- lab01/invert/matrix-3x3.h
+++ lab01/invert/matrix-3x3.h
@@ -4,8 +4,8 @@
 constexpr size_t MATRIX_SIDE = 3;
 typedef double Matrix3x3[MATRIX_SIDE][MATRIX_SIDE];
 
-bool ReadMatrix(std::istream & input, Matrix3x3 matrix);
+bool ReadMatrix(std::istream & input, Matrix3x3 & matrix);
 
-bool WriteMatrix(std::ostream & output, const Matrix3x3 matrix);
+bool WriteMatrix(std::ostream & output, Matrix3x3 const& matrix);
 
-bool InvertMatrix(const Matrix3x3 original, Matrix3x3 inverted);
+bool InvertMatrix(Matrix3x3 const& original, Matrix3x3 & inverted);
